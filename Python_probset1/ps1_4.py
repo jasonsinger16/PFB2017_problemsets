a0d52e9e498a8f14ea454940d7b59c5cc7a430b1
jasonsinger16 <jasonsingerbio@gmail.com>
2017-10-17 11:44:18 -0400

    add my solutions to Python problem set 1

---
+++ Python_probset1/ps1_4.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# prints out my 1) name, 2) favorite color, 3) favorite activity, 4) favorite animal
+# 4 arguments are passed to the executable in the command line to match the above categories
+
+import sys
+
+name = sys.argv[1]
+color = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+
+print(name)
+print(color)
+print(activity)
+print(animal)
+
