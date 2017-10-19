3a9f77a4f9c8b51b2e5dbe268f875d4dfa84d530
jasonsinger16 <jasonsingerbio@gmail.com>
2017-10-19 10:09:17 -0400

    add my solutions to probset4(5-7) working w loops and lists

---
+++ Python_probset4/ps4_07.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# A script that uses range() in a 'for' loop to print out every number between 1 and 100
+
+for no in range(1,101):
+    print(no,end=' ')
+print("\n")
---
+++ Python_probset4/ps4_07.py~
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# A script that uses range() in a 'for' loop to print out every number between 0 and 99
+
+for no in range(100):
+    print(no,end=' ')
+print("\n")
