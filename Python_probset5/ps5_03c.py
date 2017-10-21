a8893a5257effcbe185f5cf0a19ebf0995d7425a
jasonsinger16 <jasonsingerbio@gmail.com>
2017-10-20 21:44:32 -0400

    add comments to ps5_03c.py, add the start of my solution to probset7(prob1)

--- Python_probset5/ps5_03c.py
+++ Python_probset5/ps5_03c.py
@@ -7,17 +7,17 @@
 seqs_file = open("Python_05_v2.fasta", "r")
 concat_seqs = {}
 
-for line in seqs_file:
+for line in seqs_file:                          # convert wrapped fasta-format file into dictionary
     line = line.rstrip()
     if ">" in line:
         hdr = line
-        concat_seqs[hdr]=''
+        concat_seqs[hdr]=''                     # set header as key
     else:
         seq = line
-        concat_seqs[hdr]=concat_seqs[hdr]+seq
+        concat_seqs[hdr]=concat_seqs[hdr]+seq   # concatenate originally wrapped lines of sequence into a single string = value in key:value pair
 
-for khdr in concat_seqs:    
-    proc_seq = concat_seqs[khdr]
+for khdr in concat_seqs:
+    proc_seq = concat_seqs[khdr]                # replace values(sequences) in dictionary with the reverse-complement of the same seq
     subAt = proc_seq.replace('A', 't')
     subTa = subAt.replace('T', 'a')
     subCg = subTa.replace('C', 'g')
@@ -26,14 +26,14 @@ for khdr in concat_seqs:
     rev_comp = cap_allsubd[::-1]
     concat_seqs[khdr] = rev_comp
 
-for k2hdr in concat_seqs:
-    print(k2hdr + ", reverse-complement")
+for k2hdr in concat_seqs:                      # iterate over dictionary
+    print(k2hdr + ", reverse-complement")      # modify header line to denote that reverse-comp is now displayed, instead of orig seq
     long_seq = concat_seqs[k2hdr]
     linelength = 96
-    linelist = []
+    linelist = []                              # split rev-comp sequences into 96char chuncks and print out new fasta file
     for pos in range(0, len(long_seq), linelength):
-        linelist = linelist + [long_seq[pos:pos+linelength]]
-    for newline in linelist:
-        print(newline)
-
+        linelist = linelist + [long_seq[pos:pos+linelength]]   ### here, putting chunks of sequence into a list and then printing out list  
+    for newline in linelist:                                   ### but found out later that I'm adding one unnecessary step
+        print(newline)                                         ### could just break (slice) chunks off the string and print them
+                                                               ### didn't need to create the list
 seqs_file.close()
---
+++ Python_probset7/ps7_01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+# Create a function to format a string of DNA so that each line is no more than 60 nt long.
+# INPUT: a string of DNA without newlines
+# RETURNS: a string of DNA with lines no more than 60 nucleoties long
+
